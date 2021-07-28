05665574f4ebe758119ead8301ab70bc596e8dd8
vuthihathu <vuthihathu2000@gmail.com>
2021-07-28 21:59:48 +0700

    test commit

--- Backend/Controllers/CategoriesController.php
+++ Backend/Controllers/CategoriesController.php
@@ -13,33 +13,22 @@
 			$action = "index.php?controller=categories&action=updatePost&id=$id";
 			include "Views/CategoriesCreateUpdateView.php";
 		}
-		//edit khi an nut submit
 		public function updatePost(){
 			$id = isset($_GET["id"]) ? $_GET["id"] : 0;
-			//goi ham de update du lieu
 			$this->modelUpdate($id);
-			//di chuyen den trang danh sach cac ban ghi
 			echo "<script>location.href='index.php?controller=categories&action=read';</script>";
 		}
-		//create ban ghi
 		public function create(){
 			$action = "index.php?controller=categories&action=createPost";
-			//goi view
 			include "Views/CategoriesCreateUpdateView.php";
 		}
-		//create ghi an nut submit
 		public function createPost(){
-			//goi ham de insert ban ghi
 			$this->modelCreate();
-			//di chuyen den trang danh sach cac ban ghi
 			echo "<script>location.href='index.php?controller=categories&action=read';</script>";
 		}
-		//delete ban ghi
 		public function delete(){
 			$id = isset($_GET["id"]) ? $_GET["id"] : 0;
-			//goi ham de update du lieu
 			$this->modelDelete($id);
-			//di chuyen den trang danh sach cac ban ghi
 			echo "<script>location.href='index.php?controller=categories&action=read';</script>";
 		}
 	}
--- Backend/Models/CategoriesModel.php
+++ Backend/Models/CategoriesModel.php
@@ -1,14 +1,9 @@
 <?php 
 	class CategoriesModel{
-		//ham liet ke danh sach cac ban ghi, co phan trang
 		public function modelRead($recordPerPage){
-			//lay tong to so ban ghi
 			$total = $this->modelTotal();
-			//tinh so trang
 			$numPage = ceil($total/$recordPerPage);
-			//lay so trang hien tai truyen tu url
 			$page = isset($_GET["p"]) && $_GET["p"] > 0 ? $_GET["p"]-1 : 0;
-			//lay tu ban ghi nao
 			$from = $page * $recordPerPage;
 			//thuc hien truy van
 			$conn = Connection::getInstance();
