d8f45c0df68c4ad3a7e8734c033a7951440078ee
TranSonDinh <sondinh2703@gmail.com>
2021-07-28 22:32:54 +0700

    up

--- Frontend/Controllers/ProductsController.php
+++ Frontend/Controllers/ProductsController.php
@@ -3,9 +3,13 @@
     class ProductsController extends ProductsModel{
         public function categories(){
 			$category_id = isset($_GET["id"]) ? $_GET["id"] : 0;
+			//quy dinh so ban ghi tren mot trang
 			$recordPerPage = 12;
+			//tinh so trang
 			$numPage = ceil($this->modelTotal($category_id)/$recordPerPage);
+			//goi ham de lay du lieu
 			$listRecord = $this->modelRead($category_id,$recordPerPage);
+			//load view
 			include "Views/ProductsCategoriesView.php";
         }
         public function showall()
@@ -17,7 +21,9 @@
         }
         public function detail(){
 			$id = isset($_GET["id"]) ? $_GET["id"] : 0;
+			//goi ham trong class Model de lay 1 ban ghi
 			$record = $this->modelGetProduct($id);
+			//load view
 			include "Views/ProductDetailView.php";
         }
     }
