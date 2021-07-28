0f457c4db962864c92a540e961324719c449d701
vuthihathu <vuthihathu2000@gmail.com>
2021-07-28 21:18:54 +0700

    thu

--- Frontend/Controllers/ProductsController.php
+++ Frontend/Controllers/ProductsController.php
@@ -3,13 +3,9 @@
     class ProductsController extends ProductsModel{
         public function categories(){
 			$category_id = isset($_GET["id"]) ? $_GET["id"] : 0;
-			//quy dinh so ban ghi tren mot trang
 			$recordPerPage = 12;
-			//tinh so trang
 			$numPage = ceil($this->modelTotal($category_id)/$recordPerPage);
-			//goi ham de lay du lieu
 			$listRecord = $this->modelRead($category_id,$recordPerPage);
-			//load view
 			include "Views/ProductsCategoriesView.php";
         }
         public function showall()
@@ -21,12 +17,8 @@
         }
         public function detail(){
 			$id = isset($_GET["id"]) ? $_GET["id"] : 0;
-			//goi ham trong class Model de lay 1 ban ghi
 			$record = $this->modelGetProduct($id);
-			//load view
 			include "Views/ProductDetailView.php";
         }
-    
-		
     }
 ?>
