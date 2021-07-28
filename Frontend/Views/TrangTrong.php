365302e96160b66176ed3d91646e9d0d81739c6f
TranSonDinh <sondinh2703@gmail.com>
2021-07-28 22:40:02 +0700

    up

--- Frontend/Views/TrangTrong.php
+++ Frontend/Views/TrangTrong.php
@@ -27,22 +27,6 @@
     })
 </script>
 <body>
-    <!--  --> 
-    <!--Start of Tawk.to Script-->
-<!-- <script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/5f915cae17015e2aea6d60ee/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script> -->
-<!--End of Tawk.to Script-->
-    <!--  -->
-    <!-- wrapper -->
     <div class="wrapper">
     <?php 
    include "HeaderView.php";
