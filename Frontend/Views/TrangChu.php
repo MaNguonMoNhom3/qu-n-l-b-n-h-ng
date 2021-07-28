82ddc08dc39d3a7688a475d5c743a649a13b9510
TranSonDinh <sondinh2703@gmail.com>
2021-07-28 15:31:36 +0700

    up2

---
+++ Frontend/Views/TrangChu.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Men's Fashion for Gentlemen</title>
+</head>
+
+<body>
+   Hello tranh chủ nè
+</body>
+</html>
